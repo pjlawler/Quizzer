00c66fba97b61be22ac5ba87f288d8c218f087d0
Pat Lawler <patlaw777@icloud.com>
2022-01-10 20:27:23 -0600

    format with timer in upper corner

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ let currentQuestion = null;
 let answeredCorrect = null;
 let answeredWrong = null;
 let gameOver = true;
+let score = null;
+
 
 const questionBank = [
     {
@@ -24,21 +26,84 @@ const questionBank = [
     }
 ]
 
-function setQuestionText(questionNumber) {
-    // Sets the question on the page through the DOM
+function presentQuestion() {
+
     const questionEl = document.querySelector(".questionText");
-    questionEl.textContent = questionBank[questionNumber].questionText;
-}
+    let questionText = "Welcome to Quizzer! Press Start to begin.";
+    let answerText = "";
 
-function setAnswers(questionNumber) {
+    if (gameOver != true) {
+        questionText = questionBank[currentQuestion].questionText;
+    }
 
-    // Sets the multiple choice answers on the page through the DOM.  It also adds and answerID attribute to both the answer text and the div, so when the user taps it, both targets will have the id to check against the correct answer
-    for(let i = 0; i < questionBank[questionNumber].answers.length; i++) {
+    // Displays the question from the question bank into it's respectve element
+    questionEl.textContent = questionText; 
+    
+    // Displays each of the answers from the question bank into their respective elements.
+    // It also adds an answerID attribute to elements for each answer text and its wrapper div, 
+    // so when the user taps it, both targets will have the id to check against the correct answer
+    // no matter which target is clicked
+    for(let i = 0; i < 4; i++) {
+        
         const answerTextEl = document.querySelector("#answer-text-" + i);
         const answerBoxEl = document.querySelector("#answer-" + i);
         answerTextEl.setAttribute("answerId", i);
         answerBoxEl.setAttribute("answerId", i);
-        answerTextEl.textContent = questionBank[questionNumber].answers[i];
+    
+        if (gameOver != true) {
+            answerText = questionBank[currentQuestion].answers[i];           
+        }
+        else {
+            answerText = "";
+        }    
+        answerTextEl.textContent = answerText;
+    }
+}
+
+function updateScoreBoard() {
+    const scoreBoardEl = document.querySelector("#current-score");
+    scoreBoardEl.textContent = score;
+}
+
+function decrementTimer() {
+
+    if (currentQuestion == questionBank.length) {
+        // Ends the game once the last question in the questionBank is answered
+        clearInterval(gameTimer);
+        gameOver = true;
+        startEl.textContent = "Start"
+        score = score + Math.floor(timerValue * 100)
+        updateScoreBoard();
+        alert("Congratulations! You've finished the quiz before the time ran out! You've earned " + Math.floor(timerValue * 100) + " bonus timer points!");
+        presentQuestion();
+    }
+    else if (timerValue > 0 && !gameOver) {
+        // Decreases the timer by .1 second and displays on the page as long as timer is >0 and the game is still in play
+        timerValue = (timerValue - .1).toFixed(1);
+        let strTime = timerValue.toString();
+        
+        if (timerValue < 10) {
+            strTime = "0" + strTime;
+        }
+        
+        startEl.textContent = strTime;
+    }
+    else {
+        // This will execute when timer has expired or the player pressed the stop button
+        score = 0;
+        startEl.textContent = "Start"
+        clearInterval(gameTimer);
+
+        if (gameOver = true) {
+            endMessage = "You've stopped the game and gave up all your points."
+        }
+        else {
+            endMessage = "You've run out of time and lost all your points!"
+            gameOver = true;        
+        }
+        alert(endMessage);
+        updateScoreBoard();
+        presentQuestion();
     }
 }
 
@@ -54,64 +119,27 @@ function answerClicked(event) {
 
     // Gets the div element so the background can be changed to red or green
     const answeredEl = document.querySelector("#answer-" + answerId);
-
     const correctAnswer = questionBank[currentQuestion].correctAnswer;
 
     // Compares the clicked answer to the correct answer
     if (answerId == correctAnswer) {
         answeredCorrect++;
+        score = score + 10;
+        updateScoreBoard();
         answeredEl.className = "correct_answer";
     }
     else {
         answeredWrong++;
+        timerValue = timerValue - 20;
         answeredEl.className = "wrong_answer";
     }
 
     // Keeps the background the result color intermittently, then resets it back to normal before presenting the next question.
     setTimeout(() => {
         answeredEl.className = "";
+        currentQuestion++
         presentQuestion();
     }, 500);
-}
-
-
-function presentQuestion() {
-
-    currentQuestion++
-
-    if (currentQuestion == questionBank.length) {
-        gameOver = true;
-        return false;
-    }
-    setQuestionText(currentQuestion);
-    setAnswers(currentQuestion);
-}
-
-function decrementTimer() {
-    if (timerValue > 0 && !gameOver) {
-        // Decreases the timer by .1 second and displays on the page as long as timer is >0 and the game is still in play
-        timerValue = (timerValue - .1).toFixed(1);
-        let strTime = timerValue.toString();
-        
-        if (timerValue < 10) {
-            strTime = "0" + strTime;
-        }
-        
-        timerEl.innerHTML = strTime;
-    }
-    else if(gameOver) {
-        // If anything shuts the game off, then this will end the timer
-       startEl.textContent = "Start"
-       clearInterval(gameTimer);
-       console.log(timerValue);
-    }
-    else {
-        // This runs if the timer has ran down to zero
-        gameOver = true;
-        startEl.textContent = "Start"
-        clearInterval(gameTimer);
-        // Add logic for ending the game
-    }
 }
 
 function buttonClicked() {
@@ -121,13 +149,15 @@ function buttonClicked() {
         gameOver = true;
     }
     else {
-        // Resets the quiz's settings and starts over
-        startEl.textContent = "Stop" 
+        // Resets the quiz's settings and starts over for a new game
+        
+        // Resets the quiz's global variables
         gameOver = false;
         timerValue = 35;
         answeredCorrect = 0;
         answeredWrong = 0;
-        currentQuestion = -1;
+        score = 0;
+        currentQuestion = 0;
         gameTimer = setInterval(decrementTimer, 100);
         presentQuestion();
     }
@@ -136,3 +166,7 @@ function buttonClicked() {
 answerAreaEl.addEventListener("click", answerClicked);
 startEl.addEventListener("click", buttonClicked)
 
+// Things to do
+// Shuffle the question bank
+// Shuffle the answers
+// Update the UI/UX
