61382f843c7b3df743597f79dfea5a7e9c0bacd9
Pat Lawler <patlaw777@icloud.com>
2022-01-11 22:48:56 -0600

    got high score board to update and store in localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,7 @@
 const answerAreaEl = document.querySelector("#answerArea");
 const timerEl = document.querySelector("#timer");
 const startEl = document.querySelector("#start-button");
-
-let gameTimer = null;
-let timerValue = null;
-let currentQuestion = null;
-let answeredCorrect = null;
-let answeredWrong = null;
-let gameOver = true;
-let score = null;
+const resetEl = document.getElementById("reset-button");
 
 const questionBank = [
     {
@@ -29,14 +22,31 @@ const questionBank = [
     answers: ["const pi = 3.1456;", "let lightIsOn = !switchIsDown;", "let z = x === y;", "dim var = 1000;"],
     correctAnswer: 3
     }]
+let gameTimer = null;
+let timerValue = null;
+let currentQuestion = null;
+let answeredCorrect = null;
+let answeredWrong = null;
+let gameOver = true;
+let score = null;
+let leaderBoard = [];
 
 function presentQuestion() {
 
     const questionEl = document.querySelector(".questionText");
-    let questionText = "Welcome to Quizzer! Press Start to begin.";
+    let questionText = "-- Press Start to begin --";
     let answerText = "";
 
-    if (gameOver != true) {
+    if (currentQuestion == questionBank.length) {
+        // Ends the game once the last question in the questionBank is answered
+        clearInterval(gameTimer);
+        gameOver = true;
+        startEl.textContent = "Start";
+        score = score + Math.floor(timerValue * 100);
+        updateScoreBoard();
+        leaderBoardHandler();
+    }
+    else {
         questionText = questionBank[currentQuestion].questionText;
         shuffleAnswers();
     }
@@ -72,17 +82,7 @@ function updateScoreBoard() {
 
 function decrementTimer() {
 
-    if (currentQuestion == questionBank.length) {
-        // Ends the game once the last question in the questionBank is answered
-        clearInterval(gameTimer);
-        gameOver = true;
-        startEl.textContent = "Start";
-        score = score + Math.floor(timerValue * 100);
-        updateScoreBoard();
-        const initials = prompt("Congratulations! You've finished the quiz before the time ran out! You've earned " + Math.floor(timerValue * 100) + " bonus timer points for a total of " + score + " points!\n\nPlease enter your initials!");
-        presentQuestion();
-    }
-    else if (!gameOver) {
+      if (!gameOver) {
         // Decreases the timer by .1 second and displays on the page as long as timer is >0 and the game is still in play
         timerValue = (timerValue - .1).toFixed(1);
 
@@ -160,7 +160,6 @@ function buttonClicked() {
         gameOver = true;
     }
     else {
-        // Resets the quiz's settings and starts over for a new game
         
         // Resets the quiz's global variables
         gameOver = false;
@@ -170,12 +169,15 @@ function buttonClicked() {
         score = 0;
         currentQuestion = 0;
         gameTimer = setInterval(decrementTimer, 100);
+
+        // Creates the td elements with the initial and score for each element in the learder board array
         shuffleQuestionBank();
         presentQuestion();
     }
 }
 
 function shuffleQuestionBank() {
+    // Shuffles the questions in the array so they are presented in random order
     for (let i = 0; i < questionBank.length; i++) {
         const swapIndex = Math.floor(Math.random() * questionBank.length);
         [questionBank[i], questionBank[swapIndex]] = [questionBank[swapIndex], questionBank[i]];
@@ -183,6 +185,7 @@ function shuffleQuestionBank() {
 }
 
 function shuffleAnswers() {
+    // Shuffles the answers so they are presented in different positions during the quiz
     for (let i = 0; i < 4; i++) {
         const swapIndex = Math.floor(Math.random() * 4);
         if (swapIndex == questionBank[currentQuestion].correctAnswer) {
@@ -195,8 +198,74 @@ function shuffleAnswers() {
     }
 }
 
+
+
+function leaderBoardHandler() {
+    
+    // Gets the players initials and protects from more than 3 chars or no characters
+    let initials = "";
+    initials = prompt("Please enter your initials.").toUpperCase();
+    initials = (!initials) ? "???" : initials.substring(0,3);
+
+    // Adds the players initials and score to leaderboard array and then sorts it by the highest score
+    console.log(leaderBoard);
+    leaderBoard.push([initials, score]);
+    leaderBoard.sort(sortByScores);
+    
+    // Code to sort the array by the second element and in reverse order
+    function sortByScores(a, b) {
+        if (a[1] === b[1]) {
+            return 0;
+        }
+        else {
+            return (a[1] > b[1]) ? -1 : 1;
+        }
+    }
+
+    // Ensures there is no more than 10 scores listed in the array
+    while (leaderBoard.length > 10) {
+        leaderBoard.pop()
+    }
+
+    window.localStorage.setItem("scores", JSON.stringify(leaderBoard));
+
+    // Creates the td elements with the initial and score for each element in the learder board array
+    for (let i = 0; i < leaderBoard.length; i++) {
+        if (i < 10) {
+            const tableRowEl = document.getElementById("lb-" + i);
+            tableRowEl.innerHTML = "<td>" + leaderBoard[i][0] + "</td><td>" + leaderBoard[i][1] + "</td>";
+        }
+    }
+}
+
+function loadHighScores() {
+    // Resets the quiz's settings and starts over for a new game
+    console.log(leaderBoard);
+
+    // Clears any previous scores
+    for (let i = 0; i < 10; i++) {
+        const tableRowEl = document.getElementById("lb-" + i);
+        tableRowEl.innerHTML = "";
+    }
+
+    leaderBoard = window.localStorage.getItem("scores");
+    if (!leaderBoard) {
+        leaderBoard = JSON.parse(leaderBoard);
+        return false;
+    }
+ 
+    // Updates the board witht the stored scores if any
+    for (let i = 0; i < leaderBoard.length; i++) {
+        if (i < 10) {
+            const tableRowEl = document.getElementById("lb-" + i);
+            tableRowEl.innerHTML = "<td>" + leaderBoard[i][0] + "</td><td>" + leaderBoard[i][1] + "</td>";
+        }
+    }
+}
+
 answerAreaEl.addEventListener("click", answerClicked);
 startEl.addEventListener("click", buttonClicked)
+
 
 // Things to do
 // Update UI
